adb95d0ecd7bb072316767f48e687e19c74bac1a
Shemar Jones <47797339+S-TJones@users.noreply.github.com>
2020-08-09 12:40:27 -0500

    Create projects.js
    Added some of the code needed to get it to work, will continue when I get more time. So far, I've added the project array and I've made variables for each project item. Created a Event Listener to assign the values of the project-object to the corresponding variables.

---
+++ Scripts/projects.js
@@ -0,0 +1,34 @@
+
+// Array of current projects
+const projects = [{ id: 1, project: "project-1",
+                    'project-img': "/Photos/...",
+                    overview: "Lorem ipsum..."}, 
+                  { id: 2, project: "project-2",
+                    'project-img': "/Photos/...",
+                    overview: "Lorem ipsum..."}, 
+                  { id: 3, project: "project-3,
+                    'project-img': "/Photos/...",
+                    overview: "Lorem ipsum..."}, 
+                  { id: 4, project: "project-4",
+                    'project-img': "/Photos/...",
+                    overview: "Lorem ipsum..."}];
+                    
+// Get all the ID's
+const project = document.getElementById("project");
+const img = document.getElementById("project-img");
+const summary = document.getElementById("overview");
+
+const prev_btn = document.querySelector(".prev-btn");
+const next_btn = document.querySelector(".next-btn");
+
+// Holds the object from the array, representing a project 
+let current_project = 0;
+
+// DOM
+window.addEventListener("DOMContentLoaded", function() {
+    const obj = projects[current_project];
+    
+    project.textContent = obj.project;
+    img.src = obj.'project-img'
+    summary.textContent = obj.overview;
+});
