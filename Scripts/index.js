656e51f94f653ce0b6c8938bf7493bf304f5aa34
S-TJones <jones.shemar.sj@gmail.com>
2021-02-06 12:25:06 -0500

    Fixed the page-buttons

--- Scripts/index.js
+++ Scripts/index.js
@@ -4,48 +4,48 @@ var driver = function(){
 
     // Website and Terminal buttons
     page_links = document.getElementsByClassName("page-link");
+    buttons = document.getElementsByClassName("button");
+    // Anchor Tag text
+    a_tag = document.getElementsByClassName("anchor-text");
 
     // Left button
-    page_links[0].addEventListener("mouseover", function(){
-        
+    buttons[0].addEventListener("mouseover", function(){
 
         other_icon = document.getElementsByClassName("fas fa-angle-left")[0];
         other_icon.classList.remove("hide");
 
-        setTimeout(() => {  
-            // Anchor Tag text
-            a_tag = document.getElementsByClassName("anchor-text");
-            // Change Text
-            a_tag[0].innerHTML = "Website View";
-        }, 1000);
+        // Change Text
+        a_tag[0].innerHTML = "Website View";
         
     });
-    page_links[0].addEventListener("mouseout", function(){
-        
+    buttons[0].addEventListener("mouseout", function(){
 
         other_icon = document.getElementsByClassName("fas fa-angle-left")[0];
         other_icon.classList.add("hide");
 
-        // Anchor Tag text
-        a_tag = document.getElementsByClassName("anchor-text");
         // Change Text
         a_tag[0].innerHTML = "Website";
+        
     });
 
     // Right button
-    page_links[1].addEventListener("mouseover", function(){
-        // Change Text
-        page_links[1].innerHTML = "Website View";
+    buttons[1].addEventListener("mouseover", function(){
 
         other_icon = document.getElementsByClassName("fas fa-angle-right")[0];
         other_icon.classList.remove("hide");
-    });
-    page_links[1].addEventListener("mouseout", function(){
+
         // Change Text
-        page_links[1].innerHTML = "Website View";
+        a_tag[1].innerHTML = "Terminal View";
+        
+    });
+    buttons[1].addEventListener("mouseout", function(){
 
         other_icon = document.getElementsByClassName("fas fa-angle-right")[0];
         other_icon.classList.add("hide");
+
+        // Change Text
+        a_tag[1].innerHTML = "Terminal";
+        
     });
 }
 
